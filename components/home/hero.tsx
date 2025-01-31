7fc128e3ea5260e397e6faedd2680199282b4f61
GGsuii99 <123018@combertonvc.org>
2025-01-31 17:44:55 +0000

    Update hero.tsx

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -82,34 +82,14 @@ const HeroSection = React.memo(() => {
   const renderHeroContent = (): React.ReactNode => (
     <div className={HERO_STYLES.CONTENT}>
       <div className="md:mb-4 mb-2">
-        <h2 className="text-4xl seq">Hello 👋🏻</h2>
-        <h1 className="text-3xl seq">I am Ayush Singh</h1>
+        <h2 className="text-4xl seq">History of Earth Education</h2>
+        <h1 className="text-3xl seq">By GGsuii99</h1>
       </div>
       <p className="mb-4">
         <span className={HERO_STYLES.TYPED_SPAN} ref={typedSpanElement}></span>
       </p>
-      <div className="flex seq mb-5">{renderSocialLinks()}</div>
-      <div className="flex seq">
-        <Button
-          classes="mr-3"
-          type={ButtonTypes.OUTLINE}
-          name="Resume"
-          otherProps={{
-            target: "_blank",
-            rel: "noreferrer",
-          }}
-          href="/Ayush_Resume.pdf"
-        ></Button>
-        <Button
-          classes="ml-3"
-          type={ButtonTypes.PRIMARY}
-          name="Let's Talk"
-          href={SOCIAL_LINKS.topmate}
-          otherProps={{
-            target: "_blank",
-            rel: "noreferrer",
-          }}
-        ></Button>
+      
+     
       </div>
     </div>
   );
